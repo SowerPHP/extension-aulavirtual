8aa2c6ec7284fbf9247cb77bb5d11f2d6e57adf9
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-04-27 12:30:23 -0400

    Se modifica página de cursos para ser compatible com commit anterior (clasificación de cursos)

--- View/Cursos/index.php
+++ View/Cursos/index.php
@@ -1,8 +1,18 @@
-<h1>Cursos</h1> 
+<h1>Cursos</h1>
 <ul>
 <?php
-foreach ($cursos as $link => &$info) {
-    echo '<li><a href="',$_base,'/cursos',$link,'">',$info['name'],'</a></li>';
+foreach($cursos as $link => &$name) {
+    echo '<li>',"\n";
+    if (is_numeric($link)) {
+        echo '<strong>'.$name['name'].'</strong>',"\n";
+        echo '<ul style="margin-top:1em;margin-bottom:2em">',"\n";
+        foreach ($name['nav'] as $l => &$c) {
+            echo '<li><a href="',$_base,'/cursos',$l,'">',$c['name'],'</a></li>',"\n";
+        }
+        echo '</ul>',"\n";
+    } else {
+        echo '<a href="',$_base,'/cursos',$link,'">',$name['name'],'</a>',"\n";
+    }
 }
 ?>
 </ul>
